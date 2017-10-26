c0a70b6e1afe015f7139f1283f976ae32320ec3d
himanish <himanish.munjal@magicpin.in>
2017-10-26 07:31:51 +0530

    BUNDLE

--- app/src/main/java/com/example/apple/contactmessage/PhoneStateReceiver.java
+++ app/src/main/java/com/example/apple/contactmessage/PhoneStateReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.os.Bundle;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -18,11 +19,14 @@ public class PhoneStateReceiver extends BroadcastReceiver {
 	public void onReceive(final Context context, Intent intent) {
 		// This is not working when app is closed
 		//This will call customPhoneListener
-		Log.d("debug","in receiver");
-		Toast toast = Toast.makeText(context, "fuck inbroadcast", Toast.LENGTH_SHORT);
-		toast.show();
-		TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-		telephonyManager.listen(new CustomPhoneStateListener(context), PhoneStateListener.LISTEN_CALL_STATE);
+		Bundle extras = intent.getExtras();
+		if (extras != null) {
+			Log.d("debug","in receiver");
+			Toast toast = Toast.makeText(context, "fuck inbroadcast", Toast.LENGTH_SHORT);
+			toast.show();
+			TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+			telephonyManager.listen(new CustomPhoneStateListener(context), PhoneStateListener.LISTEN_CALL_STATE);
+		}
 	}
 
 }
